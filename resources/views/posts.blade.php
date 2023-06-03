151a046d49dd89f47ccfcb8825a6225ebeb7a455
Johan <joe.affandy@gmail.com>
2023-06-03 10:40:07 +0700

    Post Modal

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,12 +5,12 @@
     @foreach ($posts as $item)
         <article class="mb-5">
             <h2>
-                <a href="/posts/{{ $item['slug'] }}">
-                    {{ $item['title'] }}
+                <a href="/posts/{{ $item->slug }}">
+                    {{ $item->title }}
                 </a>
             </h2>
-            <h5>By: {{ $item['author'] }}</h5>
-            <p>{{ $item['body'] }}</p>
+            {{-- <h5>By: {{ $item['author'] }}</h5> --}}
+            <p>{{ $item->excerpt }}</p>
         </article>
     @endforeach
 
