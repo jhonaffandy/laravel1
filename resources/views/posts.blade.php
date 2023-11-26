355cd2b42be23798a12887e3f727ac2afd2f7207
Johan <joe.affandy@gmail.com>
2023-11-26 15:16:57 +0700

    membuat hero post

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -9,11 +9,19 @@
 
 <div class="card mb-3">
   <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
-      This content is a little bit longer.</p>
-    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+  <div class="card-body text-center">
+    <h3 class="card-title">{{ $posts[0]->title }}</h3>
+    <p>
+      <small class="text-muted">
+        By. <a href="/authors/{{ $posts[0]->author->username }}" class="text-decoration-none">{{
+          $posts[0]->author->name
+          }}</a> in
+        <a href="/categories/{{ $posts[0]->category->slug }}" class="text-decoration-none">{{ $posts[0]->category->name
+          }}</a> {{ $posts[0]->created_at->diffForHumans() }}
+      </small>
+    </p>
+    <p class="card-text">{{$posts[0]->excerpt}}</p>
+    <a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none btn btn-sm btn-primary">Read More..</a>
   </div>
 </div>
 
