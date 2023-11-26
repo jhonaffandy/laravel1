80411a419ff7713023504a0b00a0c63bbba8cb7e
Johan <joe.affandy@gmail.com>
2023-11-26 12:53:50 +0700

    memperbaiki halaman post & lazy eager load post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,7 @@ class PostController extends Controller
     {
         return view('posts', [
             "title" => "Halaman Posts",
-            'posts' => Post::with(['author', 'category'])->latest()->get(),
+            'posts' => Post::latest()->get(),
             // "posts" => Post::all(),
         ]);
     }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,17 +5,35 @@
 
 <h1 class="mb-5">{{ $title }}</h1>
 
-    @foreach ($posts as $item)
-        <article class="mb-5 border-bottom pb-4">
-            <h2><a href="/posts/{{ $item->slug }}" class="text-decoration-none">{{ $item->title }}</a></h2>
+@if ($posts->count())
 
-            <p>By. <a href="/authors/{{ $item->author->username }}" class="text-decoration-none">{{ $item->author->name }}</a> in <a href="/categories/{{ $item->category->slug }}" class="text-decoration-none">{{ $item->category->name }}</a></p>
-            {{-- {!! $item->body !!} --}}
+<div class="card mb-3">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
+      This content is a little bit longer.</p>
+    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+  </div>
+</div>
 
-            <p>{{ $item->excerpt }}</p>
+@else
+<p class="text-center fs-4">No post found.</p>
+@endif
 
-            <a href="/posts/{{ $item->slug }}" class="text-decoration-none">Read More..</a>
-        </article>
-    @endforeach
+@foreach ($posts as $item)
+<article class="mb-5 border-bottom pb-4">
+  <h2><a href="/posts/{{ $item->slug }}" class="text-decoration-none">{{ $item->title }}</a></h2>
 
-@endsection 
+  <p>By. <a href="/authors/{{ $item->author->username }}" class="text-decoration-none">{{ $item->author->name }}</a> in
+    <a href="/categories/{{ $item->category->slug }}" class="text-decoration-none">{{ $item->category->name }}</a>
+  </p>
+  {{-- {!! $item->body !!} --}}
+
+  <p>{{ $item->excerpt }}</p>
+
+  <a href="/posts/{{ $item->slug }}" class="text-decoration-none">Read More..</a>
+</article>
+@endforeach
+
+@endsection
