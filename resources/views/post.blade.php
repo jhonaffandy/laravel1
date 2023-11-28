784f45018d277e9265611f3c67bee48ee677ab2d
Johan <joe.affandy@gmail.com>
2023-11-28 07:06:07 +0700

    perbaiki single post tambah gambar

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,15 +3,21 @@
 @section('container')
 
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row justify-content-center mb-5">
         <div class="col-md-8">
-            <h2>{{ $post->title }}</h2>
+            <h2 class="mb-3">{{ $post->title }}</h2>
 
             <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name
                     }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{
                     $post->category->name }}</a></p>
 
-            {!! $post->body !!}
+            <img src="https://source.unsplash.com/1200x500?{{ $post->category->name }}" class="img-fluid"
+                alt="{{ $post->category->name }}">
+
+            <article class="my-4 fs-6">
+
+                {!! $post->body !!}
+            </article>
 
             <a href="/blog" class="d-block">Back to Posts</a>
 
