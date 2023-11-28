a328c95bd74f1f8e93814d8fa979f95e063da5ea
Johan <joe.affandy@gmail.com>
2023-11-28 07:01:40 +0700

    perbaiki single post

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,13 +1,31 @@
 @extends('layouts.main')
 
 @section('container')
-    <article>
-        <h2>{{ $post->title }}</h2>
-        {{-- <h5>{{ $post['author'] }}</h5> --}}
-        <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
-        {!! $post->body !!}
-    </article>
 
-    <a href="/blog" class="d-block">Back to Posts</a>
-    
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <h2>{{ $post->title }}</h2>
+
+            <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name
+                    }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{
+                    $post->category->name }}</a></p>
+
+            {!! $post->body !!}
+
+            <a href="/blog" class="d-block">Back to Posts</a>
+
+        </div>
+    </div>
+</div>
+{{-- <article>
+    <h2>{{ $post->title }}</h2>
+    <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a>
+        in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name
+            }}</a></p>
+    {!! $post->body !!}
+</article>
+
+<a href="/blog" class="d-block">Back to Posts</a> --}}
+
 @endsection
