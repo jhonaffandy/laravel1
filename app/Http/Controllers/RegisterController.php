6406f63e20b761cf6106faf86862ab591563e210
Johan <joe.affandy@gmail.com>
2023-12-03 17:41:05 +0700

    create user & enkripsi password

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -30,13 +32,19 @@ class RegisterController extends Controller
     //registering documentation 3
     public function store(Request $request)
     {
-        $request->validate([
+        $validatedData = $request->validate([
             'name' => 'required|max:255',
             'username' => ['required', 'min:3', 'max:255', 'unique:users'],
-            'email' => 'required|email|unique:users',
+            'email' => 'required|email:dns|unique:users',
             'password' => 'required|min:5|max:255'
         ]);
 
-        dd('registrasi berhasil!!!');
+        // $validatedData['password'] = bcrypt($validatedData['password']);
+        $validatedData['password'] = Hash::make($validatedData['password']);
+
+        // dd('registrasi berhasil!!!');
+        User::create($validatedData);
+
+        return redirect('/login');
     }
 }
