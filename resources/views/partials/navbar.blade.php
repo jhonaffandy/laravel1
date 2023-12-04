2bff74c95d70cdc2bc46e6468378d575a8349ee9
Johan <joe.affandy@gmail.com>
2023-12-05 06:15:36 +0700

    menerapkan middleware auth

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -23,14 +23,28 @@
 
       <ul class="navbar-nav">
         @auth
-
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+            aria-expanded="false">
+            Wellcome back, {{ Auth::user()->name }}
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="/dashboard"><i
+                  class="bi bi-layout-text-window-reverse me-1"></i>Dashboard</a>
+            </li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+            <li><a class="dropdown-item" href="#"><i class="bi bi-box-arrow-right me-1"></i>Logout</a></li>
+          </ul>
+        </li>
         @else
         <li class="nav-item">
           <a href="/login" class="nav-link {{ ($active === 'login')? 'active' :'' }}"><i
               class="bi bi-box-arrow-in-right mx-1"></i>Login</a>
         </li>
+        @endauth
       </ul>
-      @endauth
       {{-- <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
