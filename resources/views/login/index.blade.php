f37f3500b688f5b78f32a20821e7bb97b85605ed
Johan <joe.affandy@gmail.com>
2023-12-04 19:57:45 +0700

    otentikasi login

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -11,6 +11,13 @@
         </div>
         @endif
 
+        @if (session()->has('loginError'))
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            {{ session('loginError') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+        @endif
+
         <main class="form-signin">
             <h1 class="h3 mb-3 fw-normal text-center">Please Login</h1>
             <form action="/login" method="post">
